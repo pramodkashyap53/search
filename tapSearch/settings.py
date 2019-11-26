c152e8168367974afcb4fbd75cc6bb3018ce8f0f
pramodkashyap53 <pramodkashyap53@gmail.com>
2019-11-26 14:10:11 +0530

    settings changed

--- tapSearch/settings.py
+++ tapSearch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wje6i(geiljmc!9u$ks%nwkv8gj=c0!qclfk9ama@sbu!=cdc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://demo-tapsearch.herokuapp.com/']
 
