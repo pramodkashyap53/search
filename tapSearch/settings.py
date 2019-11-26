c7f6cf8a5ba31ef7575a147490cd80025fc72d41
pramodkashyap53 <pramodkashyap53@gmail.com>
2019-11-26 15:20:56 +0530

    final commit

--- tapSearch/settings.py
+++ tapSearch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wje6i(geiljmc!9u$ks%nwkv8gj=c0!qclfk9ama@sbu!=cdc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['demo-tapsearch.herokuapp.com']
 
